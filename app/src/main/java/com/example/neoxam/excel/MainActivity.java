5281100048233b1e3b720277240c84981527e81b
wassimbzn <wassimbzn6@gmail.com>
2018-03-21 17:47:21 +0100

    excel sheet reader

--- app/src/main/java/com/example/neoxam/excel/MainActivity.java
+++ app/src/main/java/com/example/neoxam/excel/MainActivity.java
@@ -1,74 +1,180 @@
 package com.example.neoxam.excel;
 
-import android.app.AlertDialog;
-import android.app.ProgressDialog;
-import android.content.DialogInterface;
-import android.os.Bundle;
-
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Iterator;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFCellStyle;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.hssf.util.HSSFColor;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
 import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
-import android.webkit.WebView;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.Toast;
 
-import java.io.File;
-import java.util.List;
-
-public class MainActivity extends Activity implements ExcelReaderListener{
-
-
-
-    private ProgressDialog progressDialog;
-
+public class MainActivity extends Activity implements OnClickListener
+{
+    Button writeExcelButton,readExcelButton;
+    static String TAG = "ExelLog";
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new AlertDialog.Builder(MainActivity.this)
-                .setMessage("message")
-                .setTitle("title")
-                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
 
-                        new FileChooser(MainActivity.this, new String[]{"xls", "xlsx"})
-                                .setFileListener(new FileChooser.FileSelectedListener() {
-                                    @Override
-                                    public void fileSelected(File file) {
-                                        progressDialog = new ProgressDialog(MainActivity.this);
-                                        progressDialog.setTitle("title");
-                                        progressDialog.setMessage("message");
-                                        progressDialog.setIndeterminate(true);
-                                        progressDialog.setCanceledOnTouchOutside(false);
+        writeExcelButton = (Button) findViewById(R.id.writeExcel);
+        writeExcelButton.setOnClickListener(this);
+        readExcelButton = (Button) findViewById(R.id.readExcel);
+        readExcelButton.setOnClickListener(this);
 
-                                        Toast.makeText(MainActivity.this, file.getName(), Toast.LENGTH_SHORT).show();
-                                        String filePath = file.getAbsolutePath();
-                                        ExcelReaderListener excelReaderListener = (ExcelReaderListener) MainActivity.this;
-
-                                        progressDialog.show();
-                                        try {
-                                            final WebView webView = new WebView(MainActivity.this);
-                                            new JSExcelReader(filePath, webView, excelReaderListener);
-                                        } catch (Exception ex) {
-                                            Log.e("Import excel error", ex.getMessage());
-                                        }
-                                    }
-                                })
-                                .showDialog();
-                    }
-                })
-                .show();
     }
-    @Override
-    public void onReadExcelCompleted(List<String> stringList) {
-        Toast.makeText(MainActivity.this, "Parse Completed", Toast.LENGTH_SHORT).show();
 
-        if (progressDialog != null && progressDialog.isShowing()) {
-            progressDialog.dismiss();
+    public void onClick(View v)
+    {
+        switch (v.getId())
+        {
+            case R.id.writeExcel:
+                saveExcelFile(this,"Classeur25.xls");
+                break;
+            case R.id.readExcel:
+                readExcelFile(this,"Classeur1.xls");
+                break;
+        }
+    }
+
+    private static boolean saveExcelFile(Context context, String fileName) {
+
+        // check if available and not read only
+        if (!isExternalStorageAvailable() || isExternalStorageReadOnly()) {
+            Log.e(TAG, "Storage not available or read only");
+            return false;
         }
 
-        // Write into DB
+        boolean success = false;
 
+        //New Workbook
+        Workbook wb = new HSSFWorkbook();
+
+        Cell c = null;
+
+        //Cell style for header row
+        CellStyle cs = wb.createCellStyle();
+        cs.setFillForegroundColor(HSSFColor.LIME.index);
+        cs.setFillPattern(HSSFCellStyle.SOLID_FOREGROUND);
+
+        //New Sheet
+        Sheet sheet1 = null;
+        sheet1 = wb.createSheet("myOrder");
+
+        // Generate column headings
+        Row row = sheet1.createRow(0);
+
+        c = row.createCell(0);
+        c.setCellValue("Item Number");
+        c.setCellStyle(cs);
+
+        c = row.createCell(1);
+        c.setCellValue("Quantity");
+        c.setCellStyle(cs);
+
+        c = row.createCell(2);
+        c.setCellValue("Price");
+        c.setCellStyle(cs);
+
+        sheet1.setColumnWidth(0, (15 * 500));
+        sheet1.setColumnWidth(1, (15 * 500));
+        sheet1.setColumnWidth(2, (15 * 500));
+
+        // Create a path where we will place our List of objects on external storage
+        File file = new File(Environment.getDataDirectory(), fileName);
+        FileOutputStream os = null;
+
+        try {
+            os = new FileOutputStream(file);
+            wb.write(os);
+            Log.w("FileUtils", "Writing file" + file);
+            success = true;
+        } catch (IOException e) {
+            Log.w("FileUtils", "Error writing " + file, e);
+        } catch (Exception e) {
+            Log.w("FileUtils", "Failed to save file", e);
+        } finally {
+            try {
+                if (null != os)
+                    os.close();
+            } catch (Exception ex) {
+            }
+        }
+        return success;
     }
 
+    private static void readExcelFile(Context context, String filename) {
+
+        if (!isExternalStorageAvailable() || isExternalStorageReadOnly())
+        {
+            Log.e(TAG, "Storage not available or read only");
+            return;
+        }
+
+        try{
+            // Creating Input Stream
+            File file = new File(Environment.getExternalStorageDirectory(), filename);
+            FileInputStream myInput = new FileInputStream(file);
+
+            // Create a POIFSFileSystem object
+            POIFSFileSystem myFileSystem = new POIFSFileSystem(myInput);
+
+            // Create a workbook using the File System
+            HSSFWorkbook myWorkBook = new HSSFWorkbook(myFileSystem);
+
+            // Get the first sheet from workbook
+            HSSFSheet mySheet = myWorkBook.getSheetAt(0);
+
+            /** We now need something to iterate through the cells.**/
+            Iterator rowIter = mySheet.rowIterator();
+
+            while(rowIter.hasNext()){
+                HSSFRow myRow = (HSSFRow) rowIter.next();
+                Iterator cellIter = myRow.cellIterator();
+                while(cellIter.hasNext()){
+                    HSSFCell myCell = (HSSFCell) cellIter.next();
+                    Log.d(TAG, "Cell Value: " +  myCell.toString());
+                    Toast.makeText(context, "cell Value: " + myCell.toString(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        }catch (Exception e){e.printStackTrace(); }
+
+        return;
+    }
+
+    public static boolean isExternalStorageReadOnly() {
+        String extStorageState = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED_READ_ONLY.equals(extStorageState)) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isExternalStorageAvailable() {
+        String extStorageState = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(extStorageState)) {
+            return true;
+        }
+        return false;
+    }
 }
